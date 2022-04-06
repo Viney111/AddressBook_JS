e09beba22dab4d3f59326f9f6fbe3c3355137030
Viney <vineykhaneja999@gmail.com>
2022-04-06 16:39:04 +0530

    [Viney] ADDED: Validating Contact Details

--- AddressBook.js
+++ AddressBook.js
@@ -1,13 +1,14 @@
 
+let firstNamePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+let lastNamePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+let addressPattern = new RegExp('^[A-Za-z0-9 /,]{4,}$');
+let cityPattern = new RegExp('^[A-Za-z]{4,}$');
+let statePattern = new RegExp('^[A-Za-z]{4,}$');
+let zipPattern = new RegExp('^[1-9]{1}[0-9]{5}$')
+let phoneNoPattern = new RegExp('^[0-9]{2}[ ][6-9]{1}[0-9]{9}');
+let eMailPattern = new RegExp('^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$')
+
 class Contact{
-    firstName;
-    lastName;
-    address;
-    city;
-    state;
-    zip;
-    phoneNumber;
-    eMail;
 
     constructor(...params){
         this.firstName = params[0];
@@ -19,10 +20,56 @@ class Contact{
         this.phoneNumber = params[6];
         this.eMail = params[7];
     }
+    get firstName() {return this._firstName};
+    set firstName(firstName){
+        if (firstNamePattern.test(firstName)) this._firstName = firstName;
+        else throw 'First Name pattern is incorrect';
+    }
+    get lastName() {return this._lastName};
+    set lastName(lastName){
+        if (lastNamePattern.test(lastName)) this._lastName = lastName;
+        else throw 'Last Name pattern is incorrect';
+    }
+    get address() {return this._address};
+    set address(address){
+        if (addressPattern.test(address)) this._address = address;
+        else throw 'Address pattern is incorrect';
+    }
+    get state() {return this._state};
+    set state(state){
+        if (statePattern.test(state)) this._state = state;
+        else throw 'State pattern is incorrect';
+    }
+    get city() {return this._city};
+    set city(city){
+        if (cityPattern.test(city)) this._city = city;
+        else throw 'City pattern is incorrect';
+    }
+    get zip() {return this._zip};
+    set zip(zip){
+        if (zipPattern.test(zip)) this._zip = zip;
+        else throw 'Zip pattern is incorrect';
+    }
+    get phoneNumber() {return this._phoneNumber};
+    set phoneNumber(phoneNumber){
+        if (phoneNoPattern.test(phoneNumber)) this._phoneNumber = phoneNumber;
+        else throw 'PhoneNumber pattern is incorrect';
+    }
+    get eMail() {return this._eMail};
+    set eMail(eMail){
+        if (eMailPattern.test(eMail)) this._eMail = eMail;
+        else throw 'Email pattern is incorrect';
+    }
 
         toString(){
             return ("FullName: "+this.firstName+this.lastName+"|Full Address: "+this.address+","+this.city+","+this.state+","+this.zip+"|Phone: "+this.phoneNumber+ "|EMail: "+this.eMail);
         }
 }
-let contactObj = new Contact("Viney","Khaneja","657/8,VPO Kalanaur","Rohtak","Haryana",124113,7206594149,"vineykhaneja999@gmail.com");
-console.log(contactObj.toString());
+
+try{
+    let contactObj = new Contact("Viney","Khaneja","657/8,VPO Kalanaur","Rohtak","Haryana",124113,"91 7206594149","vineykhaneja999@gmail.com");
+    console.log(contactObj.toString());
+}
+catch (ex){
+    console.error(ex);
+}
