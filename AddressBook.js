d45deabdb9114082962c6800af67333ae2abaae5
Viney <vineykhaneja999@gmail.com>
2022-04-06 16:06:20 +0530

    [Viney] ADDED: Creating Contact Class

---
+++ AddressBook.js
@@ -0,0 +1,28 @@
+
+class Contact{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    eMail;
+
+    constructor(...params){
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.eMail = params[7];
+    }
+
+        toString(){
+            return ("FullName: "+this.firstName+this.lastName+"|Full Address: "+this.address+","+this.city+","+this.state+","+this.zip+"|Phone: "+this.phoneNumber+ "|EMail: "+this.eMail);
+        }
+}
+let contactObj = new Contact("Viney","Khaneja","657/8,VPO Kalanaur","Rohtak","Haryana",124113,7206594149,"vineykhaneja999@gmail.com");
+console.log(contactObj.toString());
